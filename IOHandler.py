c4975c674dc42fb3ab570bb061e583c39ddd926d
jacob-parmer <jdp0061@tigermail.auburn.edu>
2020-07-26 11:39:51 -0400

    Added AudioDisplay class for handling output plots

--- IOHandler.py
+++ IOHandler.py
@@ -1,6 +1,9 @@
 import pyaudio
 import wave
 import sys
+import librosa
+import matplotlib.pyplot as plt
+import numpy as np
 
 class Audio:
 
@@ -49,7 +52,7 @@ class Audio:
 	#	  rate - audio sample rate in Hz of data to be stored
 	#
 	# OUTPUTS: N/A
-	def write_frames_to_wav(self, wave_output_filename): 
+	def write_to_wav(self, wave_output_filename): 
 		wf = wave.open(wave_output_filename, "wb")
 		wf.setnchannels(self.channels)
 		wf.setsampwidth(self.p.get_sample_size(self.ad_format))
@@ -58,5 +61,29 @@ class Audio:
 		wf.close()
 
 	def __del__(self):
-		print("Audio Closed...\n")
 		self.p.terminate()
+
+class AudioDisplay:
+
+	def __init__(self, y, sr):
+		self.y = y
+		self.sr = sr
+
+	def display_spectrogram(self):
+		S_full, phase = librosa.magphase(librosa.stft(self.y))
+		plt.figure(figsize=(12,4))
+		librosa.display.specshow(librosa.amplitude_to_db(S_full, ref=np.max),
+					 y_axis='log', x_axis='time', sr=self.sr)
+
+		plt.colorbar()
+		plt.tight_layout()
+		plt.show()
+
+	def display_chromagram(self):
+		S = np.abs(librosa.stft(self.y, n_fft=4096))**2
+		chroma_og = librosa.feature.chroma_stft(S=S, sr=self.sr)
+		chroma = librosa.decompose.nn_filter(chroma_og, aggregate=np.median,
+						     metric='cosine')
+		fig, ax = plt.subplots()
+		img = librosa.display.specshow(chroma, y_axis='chroma', x_axis='time', ax=ax)
+		plt.show()
