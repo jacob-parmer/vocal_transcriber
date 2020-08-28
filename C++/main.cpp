7b758ddeb141345490df33b7cca952012f908127
jacob-parmer <jdp0061@tigermail.auburn.edu>
2020-08-27 20:17:36 -0400

    Testing writeWAV and readWAV functions

--- C++/main.cpp
+++ C++/main.cpp
@@ -2,13 +2,14 @@
 
 int main() {
 
-	int seconds = 5;
+	int seconds = 15;
 	const int channelCount = 1;
-	const int sampleRate = 44100;
-	const int framesPerBuffer = 512;
+	const int sampleRate = 48000;
+	const int framesPerBuffer = 1024;
 	AudioUtil* ad = new AudioUtil(channelCount, sampleRate, framesPerBuffer);
-	int err = ad->record(seconds);
+	int err  = ad->record(seconds);
 	const char *filename = "recording.wav";
 	err = ad->writeWAV(filename);
+	err = ad->readWAV(filename);
 	delete ad;
 }
